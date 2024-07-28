bfa513d21771235b0e77e7e8e2e6f25f07bef7da
enoabasi <watermelonbit@gmail.com>
2024-07-28 23:51:23 +0100

    feat: sign up flow

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,10 @@
 import { SectionWrapper } from "@/components/section-wrapper";
+import { SignupForm } from "@/components/auth/signup-form";
 
 export default function SignUp() {
   return (
-    <SectionWrapper>
-      <div>
-        <p>sign up page</p>
-      </div>
+    <SectionWrapper className="flex items-center justify-center">
+      <SignupForm />
     </SectionWrapper>
   );
 }
