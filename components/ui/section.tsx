52f872b7acc4c7fa99b91cf7709256cc47b964db
enoabasi <watermelonbit@gmail.com>
2024-09-23 18:50:47 +0100

    refac: increased max width size

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -3,15 +3,17 @@ import * as React from "react";
 import { cn } from "@/lib/cn";
 
 interface Props {
+  id?: string;
   children?: React.ReactNode;
   className?: string;
 }
 
-export function Section({ children, className }: Props) {
+export function Section({ id, children, className }: Props) {
   return (
     <section
+      id={id}
       className={cn(
-        "flex flex-col items-start gap-2 px-4 py-8 mx-auto md:py-12 md:pb-8 lg:py-12 lg:pb-10 max-w-2xl lg:px-0",
+        "flex flex-col items-start gap-2 px-4 py-8 mx-auto md:py-12 md:pb-8 lg:py-12 lg:pb-10 max-w-4xl lg:px-0 scroll-m-5",
         className
       )}
     >
