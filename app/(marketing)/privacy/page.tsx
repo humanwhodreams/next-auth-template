f067ddb32a1fd27f30413e143f51ab6a16da3921
enoabasi <watermelonbit@gmail.com>
2024-09-23 18:52:45 +0100

    feat: marketing pages

---
+++ app/(marketing)/privacy/page.tsx
@@ -0,0 +1,9 @@
+import { Section } from "@/components/ui/section";
+
+export default function Page() {
+  return (
+    <Section>
+      <h1>Privacy</h1>
+    </Section>
+  );
+}
---
+++ app/(marketing)/terms/page.tsx
@@ -0,0 +1,11 @@
+import { Section } from "@/components/ui/section";
+
+export default function Page() {
+  return (
+    <>
+      <Section>
+        <h1>Terms &amp; Conditions</h1>
+      </Section>
+    </>
+  );
+}
