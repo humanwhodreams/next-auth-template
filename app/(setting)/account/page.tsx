e80df47eda8445dc2de9e4a3295ab963bb59ef02
enoabasi <watermelonbit@gmail.com>
2024-09-23 18:52:29 +0100

    feat: settings' pages

---
+++ app/(setting)/account/page.tsx
@@ -0,0 +1,15 @@
+import { Placeholder } from "@/components/place-holder";
+import { Section } from "@/components/ui/section";
+
+export default function Page() {
+  return (
+    <Section>
+      <h2>Account</h2>
+      <Placeholder />
+      <Placeholder />
+      <Placeholder />
+      <Placeholder />
+      <Placeholder />
+    </Section>
+  );
+}
---
+++ app/(setting)/profile/page.tsx
@@ -0,0 +1,15 @@
+import { Placeholder } from "@/components/place-holder";
+import { Section } from "@/components/ui/section";
+
+export default function Page() {
+  return (
+    <Section>
+      <h2>Profile</h2>
+      <Placeholder />
+      <Placeholder />
+      <Placeholder />
+      <Placeholder />
+      <Placeholder />
+    </Section>
+  );
+}
