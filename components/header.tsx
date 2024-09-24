9edce9207814b050b3c0a3b6a8c1742cc8e8c30f
enoabasi <watermelonbit@gmail.com>
2024-09-24 18:31:43 +0100

    refac: extend site's header component props

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,36 @@
 import { Brand } from "@/components/brand";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { UserButton } from "./user-button";
+import { cn } from "@/lib/cn";
 
-export function Header() {
+interface Props {
+  classes?: {
+    header?: {
+      className: string;
+    };
+    nav?: {
+      className: string;
+    };
+  };
+}
+export function Header({ classes }: Props) {
   return (
-    <header className="sticky inset-x-0 top-0 w-full">
-      <nav className="flex items-center justify-between h-16 max-w-4xl px-4 mx-auto sm:px-6 md:px-8 lg:px-0">
+    <header
+      className={cn(
+        "sticky inset-x-0 top-0 w-full",
+        classes?.header?.className
+      )}
+    >
+      <nav
+        className={cn(
+          "flex items-center justify-between h-16 max-w-4xl px-4 mx-auto sm:px-6 md:px-8 lg:px-0",
+          classes?.nav?.className
+        )}
+      >
         <Brand />
         <div className="flex items-center gap-x-2">
-          <UserButton user={{ name: "Acme Raccoon" }} />
           <ThemeToggle />
+          <UserButton user={{ name: "Acme Raccoon" }} />
         </div>
       </nav>
     </header>
