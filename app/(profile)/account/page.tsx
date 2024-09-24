175af6415903d52c3dffad677a44673443d901fd
enoabasi <watermelonbit@gmail.com>
2024-09-24 20:45:47 +0100

    refac: update the account page

--- app/(profile)/account/page.tsx
+++ app/(profile)/account/page.tsx
@@ -5,7 +5,14 @@ export default function Page() {
   return (
     <Section>
       <h2>Account</h2>
-      <DangerForm />
+      <div className="w-full p-4 mb-4 space-y-2 border rounded-md border-destructive bg-destructive/5">
+        <h3 className="text-destructive">Danger Zone</h3>
+        <p className="p">
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </p>
+        <DangerForm />
+      </div>
     </Section>
   );
 }
