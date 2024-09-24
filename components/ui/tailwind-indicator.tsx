2c4e56457872c62d99704b744f777ddc3a20f9ab
enoabasi <watermelonbit@gmail.com>
2024-09-24 18:30:43 +0100

    refac: move tw indicator to the right

--- components/ui/tailwind-indicator.tsx
+++ components/ui/tailwind-indicator.tsx
@@ -2,7 +2,7 @@ export function TailwindIndicator() {
   if (process.env.NODE_ENV === "production") return null;
 
   return (
-    <div className="fixed z-50 flex items-center justify-center h-8 px-3 text-xs font-medium rounded-md bottom-1 left-1 bg-muted text-muted-foreground">
+    <div className="fixed z-50 flex items-center justify-center h-8 px-3 text-xs font-medium rounded-md bottom-1 right-1 bg-muted text-muted-foreground">
       <div className="flex items-center sm:hidden">extra-small &mdash; xs</div>
       <div className="items-center hidden md:hidden sm:flex">
         small &mdash; sm
