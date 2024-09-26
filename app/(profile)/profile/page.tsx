11cd09de67fdac95f87fd63129035ae4ca837441
enoabasi <watermelonbit@gmail.com>
2024-09-26 10:09:30 +0100

    feat: add bio form to the profile's page

--- app/(profile)/profile/page.tsx
+++ app/(profile)/profile/page.tsx
@@ -1,3 +1,4 @@
+import { BioForm } from "@/components/forms/bio-form"
 import { EmailForm } from "@/components/forms/email-form";
 import { NameForm } from "@/components/forms/name-form";
 import { PasswordForm } from "@/components/forms/password-form";
@@ -8,6 +9,7 @@ export default function Page() {
     <Section>
       <h2>Profile</h2>
       <NameForm />
+      <BioForm />
       <EmailForm />
       <PasswordForm />
     </Section>
